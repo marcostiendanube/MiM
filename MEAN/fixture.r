627baaba8e04cecd4972b9674db35b27a4911acd
unknown <marcos@tiendanube.com>
2018-05-23 18:57:05 -0300

    TP 2 - esqueleto

---
+++ MEAN/fixture.r
@@ -0,0 +1,17 @@
+fase_de_grupo <- function(df_grupos){
+  # Agrega a df_grupo nuevas columnas para los resultados de los partidos
+  # df_grupos -> lista de los paises del mundial 2018 con su respectivo grupo
+  # pais -> nombre del pais
+  # grupo -> letra del grupo
+  # puntos -> puntos del equipo (3 x partido ganado, 1 x partido empatado)
+  # gf -> goles a favor del equipo
+  # gc -> goles en contra del equipo
+  return(df_grupos)
+}
+
+fase_eliminatoria <- function(df_grupos){
+  # Simula toda la fase de eliminacion y devuelve un vector con los paises
+  # en los puestos del 1 al 4
+  # ganadores -> c(campeon, segundo, tercero, cuarto)
+  return(ganadores)
+}
---
+++ MEAN/predictor.r
@@ -0,0 +1,12 @@
+# equipo_1: nombre del equipo 1
+# equipo_2: nombre del equipo 2
+# df_equipos: data frame con todos los equipos del mundial 2018 y sus Xs
+# modelo de prediccion ya entrenado.
+
+predictor_partido <- function (equipo_1,equipo_2,df_equipos, modelo){
+
+  # resultado <- c(quien_gana,goles_equipo_1,goles_equipo_2)
+  # quien_gana {1,2} -> 1: gana Equipo1, 2: gana Equipo2
+  # goles_equipo_n -> goles redondeado sin decimales
+  return(resultado)
+}
